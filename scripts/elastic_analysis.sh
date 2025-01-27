5a1fbdd541d3efc840ad5a22c2e9d3b9a703c512
Jeremy Gingrich <jerging27@login1.ls6.tacc.utexas.edu>
2025-01-27 14:19:45 -0600

    Removed redundant files

--- scripts/elastic_analysis.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-TYPE="2"
-DIM="3D"
-
-bash ~/scripts/elastic_batchjobs.sh
-
-sed -e "s/TYPE/$TYPE/" -e "s/DIM/$DIM/" ~/input_files/vaspkit/elastic_INPUT.in  > ./INPUT.in || { echo "Failed to modify INPUT.in for elastic calculation setup"; exit 1; }
-echo -e "02\n201" | vaspkit
